acd4c4f7e3e646d8eaf8f7e2f1f5ac2870c5aa03
devSomethingSomething <61376074+devSomethingSomething@users.noreply.github.com>
2022-05-27 13:43:20 +0200

    Add start text and state

--- src/AdventureGirl/Assets/Scripts/GameManager.cs
+++ src/AdventureGirl/Assets/Scripts/GameManager.cs
@@ -1,17 +1,56 @@
 using TMPro;
 using UnityEngine;
 
+enum GameState
+{
+    Start,
+    InProgress,
+    End
+}
+
 public class GameManager : MonoBehaviour
 {
     [SerializeField]
     private TextMeshProUGUI scoreText;
 
+    [SerializeField]
+    private TextMeshProUGUI startGameText;
+
+    [SerializeField]
+    private PlayerController playerController;
+
     private int score;
+
+    private GameState gameState;
+
+    private void Start()
+    {
+        gameState = GameState.Start;
+    }
 
     public void IncreaseScore(int points)
     {
         score += points;
 
         scoreText.text = $"Score: {score}";
+    }
+
+    private void Update()
+    {
+        StartGame();
+    }
+
+    private void StartGame()
+    {
+        if (Input.anyKeyDown && gameState == GameState.Start)
+        {
+            scoreText.gameObject.SetActive(true);
+
+            startGameText.enabled = false;
+
+            playerController.enabled = true;
+
+            gameState = GameState.InProgress;
+        }
     }
 }
